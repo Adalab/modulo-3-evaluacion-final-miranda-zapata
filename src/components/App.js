5af3553029c024a9ab5b43d4c50fe1891e58214e
miranda-zapata <miranda.zapata@gmail.com>
2022-05-04 18:18:22 +0200

    Fetch from API

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
 import '../styles/App.scss';
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 // import { Link, Route, Routes } from 'react-router-dom';
 // import { matchPath, useLocation } from 'react-router';
-// import API from '../services/API';
+import getMovieApi from '../services/movieApi';
 // import LocalStorage from '../services/localStorage';
 // import PropTypes from 'prop-types';
 
 function App() {
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    getMovieApi().then((cleanData) => {
+      setMovieData(cleanData);
+    });
+  }, []);
+
   return (
     <>
-      <h1>Hola, Mundo</h1>
+      <h1>Owen Wilson's 'wow'</h1>
     </>
   );
 }
---
+++ src/services/movieApi.js
@@ -0,0 +1,24 @@
+const callToApi = () => {
+  // Llamamos a la API
+  return fetch(
+    'https://owen-wilson-wow-api.herokuapp.com/wows/random?results=50'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Cuando responde la API podemos limpiar los datos aquí
+      const cleanData = data.map((movie) => {
+        return {
+          poster: movie.poster,
+          movie: movie.movie,
+          line: movie.full_line,
+          year: movie.year,
+          director: movie.director,
+          audio: movie.audio,
+        };
+      });
+
+      return cleanData;
+    });
+};
+
+export default callToApi;
