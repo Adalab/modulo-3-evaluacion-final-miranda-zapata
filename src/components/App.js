bce822c0bc8c7b57ea8d07e83df8e0260f7404b4
miranda-zapata <miranda.zapata@gmail.com>
2022-05-04 17:19:55 +0200

    Update main structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import '../styles/App.scss';
+// import { useState, useEffect } from 'react';
+// import { Link, Route, Routes } from 'react-router-dom';
+// import { matchPath, useLocation } from 'react-router';
+// import API from '../services/API';
+// import LocalStorage from '../services/localStorage';
+// import PropTypes from 'prop-types';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Hola, Mundo</h1>
+    </>
+  );
 }
 
 export default App;
